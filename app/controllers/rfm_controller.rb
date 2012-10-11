10a8bcac654f109d4cc653f7d0c106a023233ae4
Guruprasad <dv.guruprasad@gmail.com>
2012-10-11 10:44:06 +0530

    minor spelling correction

--- app/controllers/rfm_controller.rb
+++ app/controllers/rfm_controller.rb
@@ -1,4 +1,4 @@
-class RfpController < ApplicationController
+class RfmController < ApplicationController
     respond_to :html, :xml, :json
 
     def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
-  get "/rfp", :to => "rfp#index"
-  match "order_value" =>  "rfp#order_value_to_product"
+  get "/rfm", :to => "rfm#index"
+  match "order_value" =>  "rfm#order_value_to_product"
 end
 
