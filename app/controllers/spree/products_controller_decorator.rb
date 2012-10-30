f05e2085edcd37f4615da9ad86372fa2044f5e9c
Guruprasad <dv.guruprasad@gmail.com>
2012-10-30 11:24:08 +0530

    show promotions only for a logged in loyal user

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -17,8 +17,7 @@ Spree::ProductsController.class_eval do
 
         if(@product.out_of_stock?)
             @substitutes = SubstitutionProbability.find_substitutes_for(@product)
-            if !@substitutes.empty? && @substitutes.first[:probability] > Spree::Config.probability_threshold_for_discounts &&
-                spree_current_user.is_loyal?
+            if !@substitutes.empty? && @substitutes.first[:probability] > Spree::Config.probability_threshold_for_discounts && current_user_is_loyal?
                 @promotion = {}
                 @promotion[:product] = @substitutes.first[:product]
                 @promotion[:discount] = 10
@@ -33,5 +32,9 @@ Spree::ProductsController.class_eval do
     private
     def record_search_behavior
         UserBehavior.record_search(@product, spree_current_user, session["session_id"]) unless spree_current_user.nil?
+    end
+
+    def current_user_is_loyal?
+        !spree_current_user.nil? && spree_current_user.is_loyal?
     end
 end
