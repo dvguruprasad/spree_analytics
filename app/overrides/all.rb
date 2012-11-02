85f3b8629584dd162b3d95a0d1a365901e00dd15
kunalch <kunalch@thoughtworks.com>
2012-11-02 18:12:19 +0530

    product content layout updated

--- app/overrides/all.rb
+++ app/overrides/all.rb
@@ -1,4 +1,15 @@
+Deface::Override.new(:virtual_path => "spree/products/_cart_form",
+                     :name => "cart_form_class_change",
+                     :set_attributes => 'div#product-variants',
+                     :attributes => {:class => "columns six alpha"})
+
 Deface::Override.new(:virtual_path => "spree/products/show",
-                     :name => "show_substitutions",
-                     :insert_after => "[data-hook='product_show']",
-                     :partial => "spree/products/substitutes")
+:name => "left_content_separator_div_beginning",
+:replace => "[data-hook='product_show']",
+:partial => "spree/products/content_show")
+
+Deface::Override.new(:virtual_path => "spree/products/show",
+:name => "show_substitutions",
+:insert_after => "[data-hook='product_show']",
+:partial => "spree/products/substitutes")
+
