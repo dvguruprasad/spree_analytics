2f1f937b8a06bb245abaa8aaaba65ccd15e5324b
kunalch <kunalch@thoughtworks.com>
2012-11-08 11:55:36 +0530

    accordian tag cloud implemnted

--- app/assets/javascripts/store/spree_analytics.js
+++ app/assets/javascripts/store/spree_analytics.js
@@ -1,4 +1,8 @@
 //= require store/spree_core
 //= require highcharts
 //= require exporting
+//= require jquery.ui.core
+//= require jquery.ui.widget
+//= require jquery.ui.accordion
+
 //= require_tree .
