f52868a77927099904bfd835ba1986f4d47fce87
Guruprasad <dv.guruprasad@gmail.com>
2012-11-19 10:46:55 +0530

    minor bug fix

--- app/models/oos_substitution_probability.rb
+++ app/models/oos_substitution_probability.rb
@@ -1,6 +1,7 @@
 class OOSSubstitutionProbability < SubstitutionProbability
     def self.find_substitutes_for(product)
         substitutes = top_substitutes_for product
+        return [] if substitutes.empty?
         substitutes.first[:product].is_promotional = true if substitutes.first[:probability] > Spree::Config.probability_threshold_for_discounts
         substitutes.map{|s| s[:product]}
     end
